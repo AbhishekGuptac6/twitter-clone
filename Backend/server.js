4d6ad923c490509643cfcc1bbcf786cc109f47d4
AbhishekGuptac6 <abhhishekguptac6@gmail.com>
2025-03-21 06:45:06 +0530

    add mongo db connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import authroutes from './routes/auth.routes.js'
+import dotenv from "dotenv";
+import connectMongoDB from "./db/connectMongoDB.js";
 
+dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use('/api/auth/', authroutes)
 
-app.listen(5000, () => {
-    console.log(`server is running at http://localhost:5000`);
+app.listen(PORT, () => {
+    connectMongoDB();
+    console.log(`server is running at http://localhost:${PORT}`);
 });
 
